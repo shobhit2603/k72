5184767fefe2617f0c8ea22b60b44915b0c8fb35
shobhit2603 <shrivastavashobhit706@gmail.com>
2025-10-11 19:55:35 +0530

    Fixed Home Screen Video Component

--- components/home/HomeHeroText.jsx
+++ components/home/HomeHeroText.jsx
@@ -6,7 +6,7 @@ export const HomeHeroText = () => {
             <div className="lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[9vw] leading-[10.5vw]">The spark for</div>
             <div className="lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[9vw] leading-[10.5vw]">
                 All
-                <div className="lg:h-[7vw] h-[9vw] lg:w-[16vw] w-[20vw] rounded-full lg:-mt-5 overflow-hidden relative isolate will-change-transform [transform:translateZ(0)]">
+                <div className="HomeVideo lg:h-[7vw] h-[9vw] lg:w-[16vw] w-[20vw] rounded-full lg:-mt-5 overflow-hidden relative isolate will-change-transform [transform:translateZ(0)]">
                     <Video />
                 </div>
                 Things
